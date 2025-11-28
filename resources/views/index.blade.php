dc1629e5c1848f01ef9ba1a70f44701f658be1b0
momijiina <93751198+momijiina@users.noreply.github.com>
2025-11-28 11:19:15 +0900

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,3 @@
-<div style="width: 100%; height: calc(100vh - 100px);">
-    <iframe srcdoc="{{ $htmlContent }}" style="width: 100%; height: 100%; border: none;"></iframe>
+<div style="width: 100%; height: calc(100vh - 100px); margin: 0; padding: 0;">
+    <iframe srcdoc="{!! htmlspecialchars($htmlContent, ENT_QUOTES, 'UTF-8') !!}" style="width: 100%; height: 100%; border: none; display: block;"></iframe>
 </div>
